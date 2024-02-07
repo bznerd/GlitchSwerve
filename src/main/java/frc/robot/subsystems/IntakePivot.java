84c363107565cd4e10906bf442c7d3972d3367c0
dantesaldino1 <dantesaldino@gmail.com>
2024-02-07 16:40:16 -0500

    intake pivot using trap profiles

--- src/main/java/frc/robot/subsystems/IntakePivot.java
+++ src/main/java/frc/robot/subsystems/IntakePivot.java
@@ -3,54 +3,70 @@ package frc.robot.subsystems;
 import static frc.robot.utilities.SparkConfigurator.getSparkMax;
 
 import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.CANSparkBase;
 import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkLowLevel;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkAbsoluteEncoder.Type;
 import com.revrobotics.SparkPIDController;
-
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.kIntake.kPivot;
 
-import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
-
 public class IntakePivot extends SubsystemBase {
 
-    private CANSparkMax pivotMotor;
+  private CANSparkMax pivotMotor;
 
-    private SparkPIDController pivotPID;
+  private SparkPIDController pivotPID;
 
-    private AbsoluteEncoder pivotEncoder;
+  private AbsoluteEncoder pivotEncoder;
 
-    private ArmFeedforward pivotFF;
-    
-    //Profile Stuff
-    private TrapezoidProfile.Constraints m_constraints = new Constraints(kPivot.kProfile.maxVel, kPivot.kProfile.minVel);
-    private TrapezoidProfile m_profile = new TrapezoidProfile(m_constraints);
-    private TrapezoidProfile.State m_goal = new TrapezoidProfile.State();
-    private TrapezoidProfile.State m_setpoint = new TrapezoidProfile.State();
+  private ArmFeedforward pivotFF;
 
-    public IntakePivot(){
-        pivotMotor = getSparkMax(kPivot.pivotMotorID,CANSparkLowLevel.MotorType.kBrushless);
+  // Profile Stuff
+  private TrapezoidProfile.Constraints constraints =
+      new Constraints(kPivot.kProfile.maxVel, kPivot.kProfile.minVel);
+  private TrapezoidProfile profile = new TrapezoidProfile(constraints);
+  private TrapezoidProfile.State goal = new TrapezoidProfile.State();
+  private TrapezoidProfile.State setpoint = new TrapezoidProfile.State();
 
-        pivotEncoder = pivotMotor.getAbsoluteEncoder(Type.kDutyCycle);
-        pivotEncoder.setPositionConversionFactor(kPivot.intakePivotEncoderPositionFactor);
-        pivotEncoder.setInverted(false);
+  public IntakePivot() {
+    pivotMotor = getSparkMax(kPivot.pivotMotorID, CANSparkLowLevel.MotorType.kBrushless);
+    pivotMotor.setIdleMode(CANSparkBase.IdleMode.kBrake);
 
-        pivotPID = pivotMotor.getPIDController();
-        pivotPID.setOutputRange(kPivot.minPIDOutput, kPivot.maxPIDOutput);
-        pivotPID.setPositionPIDWrappingEnabled(false);
-        pivotPID.setP(kPivot.kP);
-        pivotPID.setD(kPivot.kD);
+    pivotEncoder = pivotMotor.getAbsoluteEncoder(Type.kDutyCycle);
+    pivotEncoder.setPositionConversionFactor(kPivot.intakePivotEncoderPositionFactor);
+    pivotEncoder.setInverted(false);
 
-        pivotFF = new ArmFeedforward(kPivot.kS, kPivot.kG, kPivot.kV, kPivot.kA);
-    }
-    //Set position input radians
-    public Command setIntakePivotPos(double posRad){
-        return this.run(() -> pivotPID.setReference(pivotFF.calculate(posRad, 0,0), ControlType.kPosition));
-    }
+    pivotPID = pivotMotor.getPIDController();
+    pivotPID.setFeedbackDevice(pivotEncoder);
+    pivotPID.setOutputRange(kPivot.minPIDOutput, kPivot.maxPIDOutput);
+    pivotPID.setP(kPivot.kP);
+    pivotPID.setD(kPivot.kD);
 
+    pivotFF = new ArmFeedforward(kPivot.kS, kPivot.kG, kPivot.kV, kPivot.kA);
+  }
+
+  private TrapezoidProfile.State calculateSetpoint(double posRad){
+    goal = new TrapezoidProfile.State(posRad,0);
+    setpoint = profile.calculate(kPivot.period, setpoint, goal);
+    return setpoint;
+  }
+
+  private boolean getDone(){
+    return goal.position == pivotEncoder.getPosition();
+  }
+
+  // Set position input radians
+  public Command setIntakePivotPos(double posRad) {
+    return this.run(
+        () -> {
+            TrapezoidProfile.State pos = calculateSetpoint(posRad);
+            pivotPID.setReference(pos.position, ControlType.kPosition,0,pivotFF.calculate(pos.position, pos.velocity));
+        })
+        .until(this::getDone);
+  }
 }
--- src/main/java/frc/robot/subsystems/IntakeRollers.java
+++ src/main/java/frc/robot/subsystems/IntakeRollers.java
@@ -4,27 +4,26 @@ import static frc.robot.utilities.SparkConfigurator.getSparkMax;
 
 import com.revrobotics.CANSparkLowLevel;
 import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.kIntake.kRollers;
-import edu.wpi.first.wpilibj.DigitalInput;
 
 public class IntakeRollers extends SubsystemBase {
 
-    private CANSparkMax intakeMotor;
-    private DigitalInput pieceCheck;
+  private CANSparkMax intakeMotor;
+  private DigitalInput pieceCheck;
 
-    public IntakeRollers(){
-        intakeMotor = getSparkMax(kRollers.rollerMotorID,CANSparkLowLevel.MotorType.kBrushless); 
-        pieceCheck = new DigitalInput(kRollers.boolSensorChannel);
-    }
+  public IntakeRollers() {
+    intakeMotor = getSparkMax(kRollers.rollerMotorID, CANSparkLowLevel.MotorType.kBrushless);
+    pieceCheck = new DigitalInput(kRollers.boolSensorChannel);
+  }
 
-    public Command runRollers(double volts){
-        return this.run(() -> intakeMotor.setVoltage(volts));
-    }
-    
-    public boolean getPieceCheck(){
-        return pieceCheck.get();
-    }
+  public Command runRollers(double volts) {
+    return this.run(() -> intakeMotor.setVoltage(volts));
+  }
+
+  public boolean getPieceCheck() {
+    return pieceCheck.get();
+  }
 }
