6a5269d9d88ff4e82bae31ecf2dd470278d0f6b5
bznerd <ben.zemp.c@gmail.com>
2024-03-09 11:00:51 -0500

    Fix driver heading on red side

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -413,6 +413,12 @@ public class Swerve extends SubsystemBase implements Logged, Characterizable {
   // Convert driver field relative speeds to chassis speeds
   public ChassisSpeeds driverToChassisSpeeds(ChassisSpeeds speeds) {
     speeds = limiter.calculate(speeds);
+    var alliance = DriverStation.getAlliance();
+    if (alliance.isPresent()) {
+      if (alliance.get() == Alliance.Red)
+        return ChassisSpeeds.fromFieldRelativeSpeeds(
+            speeds, getHeading().plus(Rotation2d.fromDegrees(180)));
+    }
     return ChassisSpeeds.fromFieldRelativeSpeeds(speeds, getHeading());
   }
 
